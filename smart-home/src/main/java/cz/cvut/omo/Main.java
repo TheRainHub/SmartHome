5a846bc3bd9e89220b422b4fdb059a260e3897d4
TheRainHub <theraintv94@gmail.com>
2026-01-03 19:08:31 +0100

    add v1 simulation 24 hour needs to be fixed

--- smart-home/src/main/java/cz/cvut/omo/Main.java
+++ smart-home/src/main/java/cz/cvut/omo/Main.java
@@ -1,5 +1,6 @@
 package cz.cvut.omo;
 
+import java.util.Random;
 import cz.cvut.omo.builder.HouseConfigurationBuilder;
 import cz.cvut.omo.devices.TV;
 import cz.cvut.omo.model.House;
@@ -8,6 +9,14 @@ import cz.cvut.omo.sport.Bicycle;
 import cz.cvut.omo.sport.SportEquipmentPool;
 import cz.cvut.omo.factory.PersonFactory;
 import cz.cvut.omo.factory.AnimalFactory;
+import cz.cvut.omo.devices.DishWasher;
+import cz.cvut.omo.devices.Stove;
+import cz.cvut.omo.event.Event;
+import cz.cvut.omo.event.EventType;
+import cz.cvut.omo.control.HouseController;
+import cz.cvut.omo.sensors.WindSensor;
+import cz.cvut.omo.sensors.MotionSensor;
+import cz.cvut.omo.sensors.TemperatureSensor;
 
 
 public class Main {
@@ -71,7 +80,71 @@ public class Main {
         Bicycle bike4 = garage.getEquipment(Bicycle.class);
 
         garage.printPool();
-        //Garage: No available Bicycle
         System.out.println("\n==================================");
+
+        System.out.println("\n===== Testing Event System (Chain of Responsibility + Observer) =====");
+        
+        house.addResident(father);
+        house.addResident(mother);
+        house.addResident(son);
+        
+        HouseController controller = new HouseController();
+        controller.initChain(house);
+        
+        System.out.println("-> Chain initialized.");
+
+        WindSensor windSensor = new WindSensor();
+        windSensor.addObserver(controller);
+        
+        MotionSensor motionSensor = new MotionSensor();
+        motionSensor.addObserver(controller);
+
+        TemperatureSensor tempSensor = new TemperatureSensor();
+        tempSensor.addObserver(controller);
+
+        System.out.println("\n===== STARTING 24-HOUR SIMULATION =====");
+        Random rand = new Random();
+
+        double currentTemp = 22.0;
+
+        for (int hour = 0; hour < 24; hour++) {
+            System.out.println("\n--- TIME: " + hour + ":00 ---");
+
+            int wind = rand.nextInt(30);
+            System.out.println("[Weather] Wind: " + wind + " km/h");
+            windSensor.setWindSpeed(wind);
+            currentTemp += (rand.nextDouble() * 4) - 2;
+            System.out.printf("[Weather] Temperature: %.1f°C%n", currentTemp);
+            tempSensor.setTemperature(currentTemp);
+
+            if (hour >= 18 && hour <= 20) {
+                if (rand.nextInt(10) > 7) { 
+                    System.out.println("[Life] Food run out during dinner!");
+                    Event foodEvent = new Event(
+                        EventType.FOOD_FINISHED, 
+                        house, 
+                        "Fridge is empty at dinner!"
+                    );
+                    controller.update(foodEvent);
+                }
+            }
+
+            if (rand.nextInt(10) > 8) {
+                motionSensor.setMotion(true);
+            }
+            house.getDevices().forEach(d -> {
+                if (d instanceof DishWasher) ((DishWasher) d).tick();
+                if (d instanceof Stove) ((Stove) d).tick();
+            });
+            
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("\n===== SIMULATION ENDED =====");
+        garage.printPool();
     }
 }
